866057a24cd56faa3ec1324e2129120502657cd8
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-06 19:38:05 -0300

    Como a imagem não estava aparecendo, adicionei ela a minha pasta "public" e chamei ela através do page utilizando seu endereço

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
 
-import logoPath from '../assets/logosemBackground.png'; // Caminho para o logo
+import logoPath from '../../assets/logosemBackground.png';// Caminho para o logo
 
 
 export default function Home() {
   return (
     <>
       <h1>Teste</h1>
-      <Image src= {logoPath} alt="RailSync Logo" width={72} height={16} />
+      <Image src= "/logosemBackground.png" alt="RailSync Logo" width={100} height={100} />
       
     </>
   );
