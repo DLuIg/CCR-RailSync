f0f16637b646ce1ba1bc9702566b080cedce470f
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-13 18:06:05 -0300

    Página de funcionalidades criada com sucesso

--- src/app/Funcionalidades/page.tsx
+++ src/app/Funcionalidades/page.tsx
@@ -1,22 +1,66 @@
-import exp from "constants"
-import Header from "../components/Header"
 
+import ButtonAcessar from "../components/ButtonAcessar/ButtonAcessar"
+import Header from "../components/Header/Header"
 
 
 
  function Funcionalidades() {
     return(
         <>
+
+
+        <main className="bg-gradient-to-b from-white to-blue-300 font-arial">
+
+
+
+
+
+
             <Header  // PASSANDO VALORES PARA MEUS PROPS DO COMPONENTE HEADER
                 leftLink="/"  // Retornar para a página inicial (apenas colocar uma BARRA)
-                rightLink="/menu/page.tsx"
+                rightLink="/Menu"
                 leftImageSrc="/assets/return.png"
                 rightImageSrc="/assets/menu.png"
                 altTextLeft="Voltar"
                 altTextRight="Menu"
                 title="Funcionalidades"
+               
             /> 
             
+            
+            <div className="flex flex-col justify-center items-center p-5 h-screen font-thin gap-20">
+                
+
+            
+                <ButtonAcessar
+                    cor="bg-blue-600"
+                    texto="Chat CCO"
+                    width="w-[350px]"
+                    height="h-[50px]"
+                    link="/ChatCCO"   
+                 />
+               
+                <ButtonAcessar
+                    cor="bg-blue-600"
+                    texto="Jornada"
+                    width="w-[350px]"
+                    height="h-[50px]"
+                    link="/BaterPonto"
+                
+                />     
+
+                <ButtonAcessar
+                    cor="bg-blue-600"
+                    texto="Integrantes"
+                    width="w-[350px]"
+                    height="h-[50px]"
+                    link="/Integrantes"
+                
+                />              
+
+
+            </div>
+        </main>
             
             
             
