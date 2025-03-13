9d857b70ceb729db69d1f6e71b2ea2d295723747
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-13 18:03:51 -0300

    Layout corrigido para utilizar font-inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header/Header";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const inter = Inter({
+   subsets: ["latin"], 
+   weight: ["300", "400", "600"] });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -26,7 +31,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased ${inter.className}`}
+        
       > 
       
         {children}
