fc3ae7ba0c95e0822b23f2b13ac048ee4e5a03be
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-13 18:03:18 -0300

    Config Tailwind atualizado

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        azulPastel: '#3742FA',
+      },
+    },
+  },
+  plugins: [],
+};
