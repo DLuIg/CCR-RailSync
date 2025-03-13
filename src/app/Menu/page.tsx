b6e773418fa0496b7bdfa4aec12d6fac4ce1dfe9
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-13 18:04:10 -0300

    Menu criado com sucesso

---
+++ src/app/Menu/page.tsx
@@ -0,0 +1,60 @@
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Menu () {
+    return(
+        <>
+           
+           <body >
+
+                <header className="flex justify-center mb-8">
+                    <Image src="/assets/logoCCR.png" alt="Logo" width={50} height={50}  />
+                </header>
+            
+                <main>
+
+
+                    <div className='flex flex-col items-center h-screen font-sans bg-blue-500  font-inter '>
+
+                        <div className='mb-8 mt-8'>
+                            <label  > 
+                                <Link href="/BaterPonto" className='text-white text-2xl font-inter hover:text-black hover:scale-110 hover:text-3xl transform transition duration-300'>Registro de Jornada</Link>
+                            </label>
+                                
+                        </div>
+
+                        <div className='w-full border-b-2 border-black'></div>
+
+                        <div className='mb-8 mt-8'>
+                            <label  > 
+                            <Link href="/Funcionalidades" className='text-white text-2xl font-inter hover:text-black hover:scale-110 hover:text-3xl transform transition duration-300'>Funcionalidades</Link>
+                            </label>
+
+                        </div>
+
+
+                        <div className='w-full border-b-2 border-black'></div>
+                        
+
+                        <div className='mb-8 mt-8'>
+                            <label className='w-5' > 
+                            <Link href="/Integrantes" className='text-white text-2xl font-inter hover:text-black hover:scale-110 hover:text-3xl transform transition duration-300'>Quem somos?</Link>
+                            </label>
+                        </div>
+
+                        <div className='w-full border-b-2 border-black'></div>
+
+                    </div>
+
+                   
+
+
+                </main>
+
+           </body>
+        
+            
+        </>
+    )
+}
