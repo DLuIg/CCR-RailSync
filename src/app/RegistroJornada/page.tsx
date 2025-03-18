daf12eb772880cca5cfe82849a61d87686817255
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-17 23:34:48 -0300

    Imagem de elipse adicionada (simula o reconhecimento facial )

--- src/app/RegistroJornada/page.tsx
+++ src/app/RegistroJornada/page.tsx
@@ -1,24 +1,37 @@
 import Header from "../components/Header/Header"
-
+import Image from "next/image"
 
 export default function RegistroJornada(){
     return(
         <>
-            <main>
+            <main className="bg-gradient-to-r from-indigo-500 to-teal-400" >
                 <Header  // PASSANDO VALORES PARA MEUS PROPS DO COMPONENTE HEADER
-                leftLink="/Funcionalidades"  // Retornar para a página inicial (apenas colocar uma BARRA)
+                leftLink="/BaterPonto"  // Retornar para a página inicial (apenas colocar uma BARRA)
                 rightLink="/Menu"
                 leftImageSrc="/assets/return.png"
                 rightImageSrc="/assets/menu.png"
                 altTextLeft="Voltar"
                 altTextRight="Menu"
-                title="Funcionalidades"
+                title="Bater Ponto"
                 /> 
 
-                <div>
+                
 
+                    
 
+                <div className="flex flex-col justify-center items-center p-5 h-screen gap-20">
 
+                    <Image 
+                        src="/assets/Ellipse.png"
+                        alt="Elipse"
+                        width={300}
+                        height={200}
+                        className="hover:scale-110 transition duration-300 ease-in-out"> 
+                    
+                        
+                    </Image>
+                    
+                    
                 </div>
             </main>
 
