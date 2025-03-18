ad786f2291909fd5a2869406125b30ddd1eaa3db
Davi Cavalcanti <jorgedavi2709@gmail.com>
2025-03-18 11:59:26 -0300

    Página de integrantes feita a responsividade. Icons do github e linkedin adicionados.

--- src/app/Integrantes/page.tsx
+++ src/app/Integrantes/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Header from '../components/Header/Header';
+import Link from 'next/link';
 
 export default function Integrantes() {
     return (
@@ -15,51 +16,114 @@ export default function Integrantes() {
                 title="Quem somos? "
             />
 
-            <main className='flex flex-col justify-center items-center p-5 h-screen font-thin bg-linear-to-r/oklab from-indigo-500 to-teal-400'>
-                <div className='boxIntegrantes flex flex-wrap justify-center gap-9'>
-                    <div className='cardIntegrante text-lg rounded-lg hover:border-2 hover:border-white hover:shadow-lg transition duration-300 ease-in-out'>
+            <main className='flex flex-col justify-center items-center p-5 min-h-screen font-thin bg-gradient-to-r from-indigo-500 to-teal-400'>
+                <div className='boxIntegrantes flex flex-wrap justify-center gap-10 mx-auto'>
+                    <div className='cardIntegrante w-full max-w-xs text-lg rounded-lg p-4 bg-white shadow-md'>
                         <Image 
                             src="/assets/lucas_higuti.jpg" 
-                            width={200} 
-                            height={200} 
+                            width={128} 
+                            height={128} 
                             alt="Lucas" 
-                            className='w-36 sm:w-40 lg:w-48 h-36 sm:h-40 lg:h-48 object-cover rounded-full imgIntegrantes'
+                            className='w-32 sm:w-36 h-32 sm:h-36 object-cover rounded-full mx-auto transition duration-300 ease-in-out hover:shadow-lg hover:scale-105'
                         />
                         <div className="text-center mt-3">
-                            <h1 className="text-base sm:text-lg lg:text-xl">Lucas Higuti Fontanezi</h1>
+                            <h1 className="text-base sm:text-lg">Lucas Higuti Fontanezi</h1>
                             <p className="text-sm sm:text-base">RM: 561120</p>
                         </div>
-                    </div>
+                        <div className='flex justify-center gap-4 mt-4'>
+                            <Link href="https://github.com/hlucass05">
+                                <Image
+                                src="/assets/Github.png"
+                                alt='Github'
+                                width={30}
+                                height={30}
+                                className='hover:opacity-80'
+                                />
+                            </Link>
 
-                    <div className="cardIntegrante text-lg rounded-lg hover:border-2 hover:border-white hover:shadow-lg transition duration-300 ease-in-out">
-                        <Image
-                            src="/assets/mar.png"
-                            width={200}
-                            height={200}
-                            alt="Davi"
-                            className='w-36 sm:w-40 lg:w-48 h-36 sm:h-40 lg:h-48 object-cover rounded-full imgIntegrantes'
-                        />
-                        <div className="text-center mt-3">
-                            <h1 className="text-base sm:text-lg lg:text-xl">Davi Cavalcanti Jorge</h1>
-                            <p className="text-sm sm:text-base">RM: 559873</p>
+                            <Link href="https://www.linkedin.com/in/igor-neris-soares-alves-197a232b5/">
+                                <Image
+                                src="/assets/iconLinkedin.png"
+                                alt='Linkedin'
+                                width={30}
+                                height={30}
+                                className='hover:opacity-80'
+                                />
+                            </Link>
                         </div>
                     </div>
 
-                    <div className='cardIntegrante text-lg rounded-lg hover:border-2 hover:border-white hover:shadow-lg transition duration-300 ease-in-out'>
-                        <Image 
-                            src="/assets/igao.jpg" 
-                            width={200} 
-                            height={200} 
-                            alt="Igor"
-                            className='w-36 sm:w-40 lg:w-48 h-36 sm:h-40 lg:h-48 object-cover rounded-full imgIntegrantes'
+                    <div className="cardIntegrante w-full max-w-xs text-lg rounded-lg p-4 bg-white shadow-md">
+                        <Image
+                            src="/assets/mar.png"
+                            width={128} 
+                            height={128} 
+                            alt="Davi"
+                            className='w-32 sm:w-36 h-32 sm:h-36 object-cover rounded-full mx-auto transition duration-300 ease-in-out hover:shadow-lg hover:scale-105'
                         />
                         <div className="text-center mt-3">
-                            <h1 className="text-base sm:text-lg lg:text-xl">Igor Neris Soares</h1>
+                            <h1 className="text-base sm:text-lg">Davi Cavalcanti Jorge</h1>
+                            <p className="text-sm sm:text-base">RM: 559873</p>
+                        </div>
+                        <div className='flex justify-center gap-4 mt-4'>
+                            <Link href="https://github.com/daca2709">
+                                <Image
+                                src="/assets/Github.png"
+                                alt='Github'
+                                width={30}
+                                height={30}
+                                className='hover:opacity-80'
+                                />
+                            </Link>
+
+                            <Link href="https://www.linkedin.com/in/davi-jorge-468186214/">
+                                <Image
+                                src="/assets/iconLinkedin.png"
+                                alt='Linkedin'
+                                width={30}
+                                height={30}
+                                className='hover:opacity-80'
+                                />
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className='cardIntegrante w-full max-w-xs text-lg rounded-lg p-4 bg-white shadow-md'>
+                        <Image 
+                            src="/assets/igao.jpg" 
+                            width={128} 
+                            height={128} 
+                            alt="Igor"
+                            className='w-32 sm:w-36 h-32 sm:h-36 object-cover rounded-full mx-auto transition duration-300 ease-in-out hover:shadow-lg hover:scale-105'
+                        />
+                        <div className="text-center mt-3">
+                            <h1 className="text-base sm:text-lg">Igor Neris Soares</h1>
                             <p className="text-sm sm:text-base">RM: 560088</p>
+                        </div>
+                        <div className='flex justify-center gap-4 mt-4'>
+                            <Link href="https://github.com/Igo00r">
+                                <Image
+                                src="/assets/Github.png"
+                                alt='Github'
+                                width={30}
+                                height={30}
+                                className='hover:opacity-80'
+                                />
+                            </Link>
+
+                            <Link href="https://www.linkedin.com/in/igor-neris-soares-alves-197a232b5/">
+                                <Image
+                                src="/assets/iconLinkedin.png"
+                                alt='Linkedin'
+                                width={30}
+                                height={30}
+                                className='hover:opacity-80'
+                                />
+                            </Link>
                         </div>
                     </div>
                 </div>
             </main>
         </>
-    )
+    );
 }
