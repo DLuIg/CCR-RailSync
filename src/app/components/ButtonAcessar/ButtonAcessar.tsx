5ebed6f2e1f9543bb5ee48fd75e8cee94134b139
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-17 23:37:42 -0300

    Respomsividade criada para nosso component ButtonAcessar

--- src/app/components/ButtonAcessar/ButtonAcessar.tsx
+++ src/app/components/ButtonAcessar/ButtonAcessar.tsx
@@ -3,28 +3,35 @@ import Link from 'next/link';
 interface ButtonAcessarProps {
   cor: string; // Ex: 'bg-blue-600'
   texto: string; // Ex: 'Acessar'
-  width: string;
-  height:string; // Ex: 'text-lg'
-  link: string; // Ex: '/funcionalidades'
+  width?: string; // Tornado opcional
+  height?: string; // Tornado opcional
+  link?: string; // Ex: '/funcionalidades'
 }
 
 function ButtonAcessar({
   cor,
   texto,
-  width,
-  height,
+  width = 'w-full sm:w-auto', // Default para responsividade
+  height = 'text-lg sm:text-xl', // Adicionada responsividade para o texto
   link,
 }: ButtonAcessarProps) {
+  const buttonElement = (
+    <button
+      className={`${cor} ${width} ${height} py-2 sm:py-3 px-4 sm:px-5 rounded-md transition duration-300 ease-in-out text-black font-medium hover:bg-gray-300 cursor-pointer hover:shadow-lg hover:scale-105`}
+    >
+      {texto}
+    </button>
+  );
+
   return (
     <div>
-      <Link href={link} passHref>
-        <button
-          className={
-            `buttonAcessar ${cor} ${width} ${height} py-2 px-4 rounded-md transition duration-300 ease-in-out text-white font-thin hover:bg-blue-900 cursor-pointer hover:shadow-lg hover:scale-105`} // Configuração padrão para nosso button!
-        >
-          {texto}
-        </button>
-      </Link>
+      {link ? (
+        <Link href={link} passHref>
+          {buttonElement}
+        </Link>
+      ) : (
+        buttonElement
+      )}
     </div>
   );
 }
