27fff0d4c73decac46c47baf5ed134d1221981a2
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-17 23:37:09 -0300

    Criado nossa página de contato com o CCO

--- src/app/ChatCCO/page.tsx
+++ src/app/ChatCCO/page.tsx
@@ -2,35 +2,74 @@ import React from "react"
 import Header from "../components/Header/Header"
 import ButtonMsg from "../components/ButtonMsg/ButtonMsg"
 
-export default function ChatCCO(){
+export default function ChatCCO() {
     return(
         <>
             <main>
-                <Header  // PASSANDO VALORES PARA MEUS PROPS DO COMPONENTE HEADER
-                leftLink="/Funcionalidades"  // Retornar para a página inicial (apenas colocar uma BARRA)
-                rightLink="/Menu"
-                leftImageSrc="/assets/return.png"
-                rightImageSrc="/assets/menu.png"
-                altTextLeft="Voltar"
-                altTextRight="Menu"
-                title="Chat CCO"
+                <Header 
+                    leftLink="/Funcionalidades"
+                    rightLink="/Menu"
+                    leftImageSrc="/assets/return.png"
+                    rightImageSrc="/assets/menu.png"
+                    altTextLeft="Voltar"
+                    altTextRight="Menu"
+                    title="Chat CCO"
                 /> 
 
-                <div className="flex flex-col justify-center items-center p-5 h-screen font-thin bg-linear-to-r/oklab from-indigo-500 to-teal-400 gap-20">
-                   
-                    <ButtonMsg
-                    text="Iniciar"
-                    width="w-[350px]"
-                    height="h-[50px]"
-                    modalMessage="/ChatCCO"
-                    />
+                <div className="flex flex-col justify-center items-center p-5 h-screen bg-gradient-to-r from-indigo-500 to-teal-400 gap-15">
+                    <div className="grid grid-cols-2 gap-15">
+                        <ButtonMsg
+                            text="Portas com problemas"
+                            width="w-[150px]"
+                            height="h-[60px]"
+                            modalMessage="portas com problemas"
+                        />
+                        <ButtonMsg
+                            text="Emergência Médica"
+                            width="w-[150px]"
+                            height="h-[60px]"
+                            modalMessage="emergência Médica"
+                        />
+                        <ButtonMsg
+                            text="Falhas técnicas"
+                            width="w-[150px]"
+                            height="h-[60px]"
+                            modalMessage="falhas técnicas"
+                        />
+                        <ButtonMsg
+                            text="Problemas na via"
+                            width="w-[150px]"
+                            height="h-[60px]"
+                            modalMessage="problemas na via"
+                        />
+                        <ButtonMsg
+                            text="Objeto/Pessoas na via"
+                            width="w-[150px]"
+                            height="h-[60px]"
+                            modalMessage="objeto/pessoas na via"
+                        />
+                        <ButtonMsg
+                            text="Anomalia nos freios"
+                            width="w-[150px]"
+                            height="h-[60px]"
+                            modalMessage="anomalia nos freios"
+                        />
+                        <ButtonMsg
+                            text="Falha na comunicação (Rádio)"
+                            width="w-[150px]"
+                            height="h-[80px]"
+                            modalMessage="falha na comunicação (Rádio)"
+                        />
 
-                   
-
-
+                        <ButtonMsg
+                            text="Alerta de segurança"
+                            width="w-[150px]"
+                            height="h-[60px]"
+                            modalMessage="alerta de segurança"
+                        />
+                    </div>
                 </div>
             </main>
-
         </>
     )
 }
