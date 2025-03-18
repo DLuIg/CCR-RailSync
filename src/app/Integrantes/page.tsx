79b6c434db649154a1458f6b9c419cfc19691529
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-17 23:39:20 -0300

    Responsividade para página de integrantes criada

--- src/app/Integrantes/page.tsx
+++ src/app/Integrantes/page.tsx
@@ -2,71 +2,64 @@ import React from 'react';
 import Image from 'next/image';
 import Header from '../components/Header/Header';
 
-
-
-
-
 export default function Integrantes() {
-    return(
-
+    return (
         <>
             <Header
-            leftLink="/Funcionalidades"  // Retornar para a página inicial (apenas colocar uma BARRA)
-            rightLink="/Menu"
-            leftImageSrc="/assets/return.png"
-            rightImageSrc="/assets/menu.png"
-            altTextLeft="Voltar"
-            altTextRight="Menu"
-            title="Quem somos? "
-            
-            ></Header>
-            
+                leftLink="/Funcionalidades"  
+                rightLink="/Menu"
+                leftImageSrc="/assets/return.png"
+                rightImageSrc="/assets/menu.png"
+                altTextLeft="Voltar"
+                altTextRight="Menu"
+                title="Quem somos? "
+            />
+
             <main className='flex flex-col justify-center items-center p-5 h-screen font-thin bg-linear-to-r/oklab from-indigo-500 to-teal-400'>
-                <div className='boxIntegrantes flex justify-center font-sans gap-9 '>   
+                <div className='boxIntegrantes flex flex-wrap justify-center gap-9'>
                     <div className='cardIntegrante text-lg rounded-lg hover:border-2 hover:border-white hover:shadow-lg transition duration-300 ease-in-out'>
                         <Image 
-                        src="/assets/lucas_higuti.jpg" 
-                        width={200} 
-                        height={200} 
-                        alt="Lucas" 
-                        className='w-50 h-50  object-cover rounded-full imgIntegrantes' />
+                            src="/assets/lucas_higuti.jpg" 
+                            width={200} 
+                            height={200} 
+                            alt="Lucas" 
+                            className='w-36 sm:w-40 lg:w-48 h-36 sm:h-40 lg:h-48 object-cover rounded-full imgIntegrantes'
+                        />
                         <div className="text-center mt-3">
-                            <h1>Lucas Higuti Fontanezi</h1>
-                            <p>RM: 561120</p>
+                            <h1 className="text-base sm:text-lg lg:text-xl">Lucas Higuti Fontanezi</h1>
+                            <p className="text-sm sm:text-base">RM: 561120</p>
                         </div>
                     </div>
 
-                    <div className="cardIntegrante text-lg rounded-lg hover:border-2 hover:border-white  hover:shadow-lg transition duration-300 ease-in-out">
+                    <div className="cardIntegrante text-lg rounded-lg hover:border-2 hover:border-white hover:shadow-lg transition duration-300 ease-in-out">
                         <Image
                             src="/assets/mar.png"
                             width={200}
                             height={200}
                             alt="Davi"
-                            className="w-[200px] h-[200px] object-cover rounded-full imgIntegrantes"
+                            className='w-36 sm:w-40 lg:w-48 h-36 sm:h-40 lg:h-48 object-cover rounded-full imgIntegrantes'
                         />
                         <div className="text-center mt-3">
-                            <h1>Davi Cavalcanti Jorge</h1>
-                            <p>RM: 559873</p>
-                        </div>
-                        </div>
-
-
-                    <div className='cardIntegrante text-lg rounded-lg hover:border-2 hover:border-white  hover:shadow-lg transition duration-300 ease-in-out'>
-                        <Image 
-                        src="/assets/igao.jpg" 
-                        width={200} 
-                        height={200} 
-                        alt="Igor" 
-                        className='w-50 h-50 object-cover rounded-full imgIntegrantes' />
-                        <div className="text-center mt-3">
-                            <h1>Igor Neris Soares</h1>
-                            <p>RM: 560088</p>
+                            <h1 className="text-base sm:text-lg lg:text-xl">Davi Cavalcanti Jorge</h1>
+                            <p className="text-sm sm:text-base">RM: 559873</p>
                         </div>
                     </div>
 
+                    <div className='cardIntegrante text-lg rounded-lg hover:border-2 hover:border-white hover:shadow-lg transition duration-300 ease-in-out'>
+                        <Image 
+                            src="/assets/igao.jpg" 
+                            width={200} 
+                            height={200} 
+                            alt="Igor"
+                            className='w-36 sm:w-40 lg:w-48 h-36 sm:h-40 lg:h-48 object-cover rounded-full imgIntegrantes'
+                        />
+                        <div className="text-center mt-3">
+                            <h1 className="text-base sm:text-lg lg:text-xl">Igor Neris Soares</h1>
+                            <p className="text-sm sm:text-base">RM: 560088</p>
+                        </div>
+                    </div>
                 </div>
             </main>
         </>
     )
 }
-    
