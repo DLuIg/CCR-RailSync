742a2eaf9974d9426f9855290819f44ea1d8932a
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-17 23:38:46 -0300

    Menu refatorado e adicionado novo acesso

--- src/app/Menu/page.tsx
+++ src/app/Menu/page.tsx
@@ -6,6 +6,9 @@ export default function Menu () {
     return(
         <>
            
+
+
+
            <body >
 
                 <header className="flex justify-center mb-8">
@@ -17,33 +20,50 @@ export default function Menu () {
 
                     <div className='flex flex-col items-center h-screen  bg-linear-to-r/oklab from-indigo-500 to-teal-400  font-inter '>
 
-                        <div className='mb-8 mt-8'>
+                        <div className='mb-8 mt-8 transform transition duration-300 ease-in-out hover:scale-110'>
                             <label  > 
-                                <Link href="/BaterPonto" className='text-white text-2xl font-inter hover:text-black hover:scale-110 hover:text-3xl transform transition duration-300'>Registro de Jornada</Link>
+                                <Link href="/BaterPonto" className='text-white text-2xl font-inter hover:text-gray-300 hover:scale-110 hover:text-3xl '>Registro de Jornada</Link>
                             </label>
                                 
                         </div>
 
                         <div className='w-full border-b-2 border-black'></div>
 
-                        <div className='mb-8 mt-8'>
-                            <label  > 
-                            <Link href="/Funcionalidades" className='text-white text-2xl font-inter hover:text-black hover:scale-110 hover:text-3xl transform transition duration-300'>Funcionalidades</Link>
-                            </label>
-
-                        </div>
-
-
-                        <div className='w-full border-b-2 border-black'></div>
-                        
-
-                        <div className='mb-8 mt-8'>
+                        <div className='mb-8 mt-8 transform transition duration-300 ease-in-out hover:scale-110'>
                             <label className='w-5' > 
-                            <Link href="/Integrantes" className='text-white text-2xl font-inter hover:text-black hover:scale-110 hover:text-3xl transform transition duration-300'>Quem somos?</Link>
+                            <Link href="/Integrantes" className='text-white text-2xl font-inter hover:text-gray-300 hover:scale-110 hover:text-3xl '>Quem somos?</Link>
                             </label>
                         </div>
 
                         <div className='w-full border-b-2 border-black'></div>
+
+                        <div className='mb-8 mt-8 transform transition duration-300 ease-in-out hover:scale-110'>
+                            <label  > 
+                            <Link href="/ChatCCO" className='text-white text-2xl font-inter hover:text-gray-300 hover:scale-110 hover:text-3xl  '>ChatCCO</Link>
+                            </label>
+
+                        </div>
+                        <div className='w-full border-b-2 border-black'></div>
+
+
+
+
+                        <div className='mb-8 mt-8 transform transition duration-300 ease-in-out hover:scale-110'>
+                            <label  > 
+                            <Link href="/Funcionalidades" className='text-white text-2xl font-inter hover:text-gray-300 hover:scale-110 hover:text-3xl  '>Funcionalidades</Link>
+                            </label>
+
+                        </div>
+
+
+
+
+
+                        <div className='w-full border-b-2 border-black'></div>
+
+
+
+
 
                     </div>
 
