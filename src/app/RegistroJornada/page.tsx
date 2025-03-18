3c7bf34f2ef87c8d333d4a2f735a6ce7393889d3
Davi Cavalcanti <jorgedavi2709@gmail.com>
2025-03-18 12:00:31 -0300

    Responsividade da elipse (falta arrumar)

--- src/app/RegistroJornada/page.tsx
+++ src/app/RegistroJornada/page.tsx
@@ -1,40 +1,30 @@
 import Header from "../components/Header/Header"
 import Image from "next/image"
 
-export default function RegistroJornada(){
-    return(
+export default function RegistroJornada() {
+    return (
         <>
-            <main className="bg-gradient-to-r from-indigo-500 to-teal-400" >
-                <Header  // PASSANDO VALORES PARA MEUS PROPS DO COMPONENTE HEADER
-                leftLink="/BaterPonto"  // Retornar para a página inicial (apenas colocar uma BARRA)
-                rightLink="/Menu"
-                leftImageSrc="/assets/return.png"
-                rightImageSrc="/assets/menu.png"
-                altTextLeft="Voltar"
-                altTextRight="Menu"
-                title="Bater Ponto"
-                /> 
+            <main className="bg-gradient-to-r from-indigo-500 to-teal-400">
+                <Header
+                    leftLink="/BaterPonto"
+                    rightLink="/Menu"
+                    leftImageSrc="/assets/return.png"
+                    rightImageSrc="/assets/menu.png"
+                    altTextLeft="Voltar"
+                    altTextRight="Menu"
+                    title="Bater Ponto"
+                />
 
-                
-
-                    
-
-                <div className="flex flex-col justify-center items-center p-5 h-screen gap-20">
-
-                    <Image 
+                <div className="flex flex-col justify-center items-center p-5 h-screen gap-20 sm:gap-10">
+                    <Image
                         src="/assets/Ellipse.png"
                         alt="Elipse"
                         width={300}
                         height={200}
-                        className="hover:scale-110 transition duration-300 ease-in-out"> 
-                    
-                        
-                    </Image>
-                    
-                    
+                        className="hover:scale-110 transition duration-300 ease-in-out sm:w-64 sm:h-44"
+                    />
                 </div>
             </main>
-
         </>
     )
 }
