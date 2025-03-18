44f1ad818865d4ef8ad00f4e6da3f7a0f854734b
Davi Jorge <jorgedavi2709@gmail.com>
2025-03-18 20:06:12 -0300

    Elipse ajustada para responsividade

--- src/app/RegistroJornada/page.tsx
+++ src/app/RegistroJornada/page.tsx
@@ -15,13 +15,13 @@ export default function RegistroJornada() {
                     title="Bater Ponto"
                 />
 
-                <div className="flex flex-col justify-center items-center p-5 h-screen gap-20 sm:gap-10">
+                <div className="flex flex-col justify-center items-center p-15 h-screen gap-20 sm:gap-10">
                     <Image
                         src="/assets/Ellipse.png"
                         alt="Elipse"
-                        width={300}
+                        width={200}
                         height={200}
-                        className="hover:scale-110 transition duration-300 ease-in-out sm:w-64 sm:h-44"
+                        className="   hover:scale-110  transition duration-300 ease-in-out "
                     />
                 </div>
             </main>
